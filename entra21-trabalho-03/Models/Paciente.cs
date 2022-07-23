e46be1c4369661fa1fa9fe6dd9d37cc311e5866b
Wellington Scaburri <wscaburri@gmail.com>
2022-07-23 19:37:53 -0300

    Criado Models Paciente

---
+++ entra21-trabalho-03/Models/Paciente.cs
@@ -0,0 +1,12 @@
+﻿namespace entra21_trabalho_03.Models
+{
+    internal class Paciente
+    {
+        public int Id { get; set; }
+        public string NomeCompleto { get; set; }
+        public int Cpf { get; set; }
+        public int Rg { get; set; }
+        public string LocalNascimento { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
