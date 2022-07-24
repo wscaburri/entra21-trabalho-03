0caccdd41363ba7ffa8ca32b5cb4b1b3ce686ed0
Wellington Scaburri <wscaburri@gmail.com>
2022-07-24 14:17:08 -0300

    Criado Models Cliente

--- entra21-trabalho-03/Models/Cliente.cs
+++ entra21-trabalho-03/Models/Cliente.cs
@@ -1,12 +1,13 @@
 ﻿namespace entra21_trabalho_03.Models
 {
-    internal class Paciente
+    internal class Cliente
     {
         public int Id { get; set; }
         public string NomeCompleto { get; set; }
         public int Cpf { get; set; }
         public int Rg { get; set; }
-        public string LocalNascimento { get; set; }
         public DateTime DataNascimento { get; set; }
+        public string Endereco { get; set; }
+        public int Cep { get; set; }
     }
 }
