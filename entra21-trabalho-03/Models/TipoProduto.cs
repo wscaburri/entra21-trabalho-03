cde9e51aa55e75a525d3e5e2319e249e92ebcc6f
Amanda Caroline Bauler Mota <amanda.bauler@hotmail.com>
2022-07-24 14:44:42 -0300

    Inserido a classe TipoProduto

---
+++ entra21-trabalho-03/Models/TipoProduto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entra21_trabalho_03.Models
+{
+    public class TipoProduto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Observacao { get; set; }
+    }
+}
