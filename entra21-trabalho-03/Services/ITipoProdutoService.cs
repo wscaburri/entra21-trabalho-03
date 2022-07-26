6061f1418922b608549d022c15eaef77692bdcf5
Amanda Caroline Bauler Mota <amanda.bauler@hotmail.com>
2022-07-26 18:29:10 -0300

    Implementada classe interface Tipo Produto

--- entra21-trabalho-03/Services/ITipoProdutoService.cs
+++ entra21-trabalho-03/Services/ITipoProdutoService.cs
@@ -1,6 +1,13 @@
-﻿namespace entra21_trabalho_03.Services
+﻿using entra21_trabalho_03.Models;
+
+namespace entra21_trabalho_03.Services
 {
     internal interface ITipoProdutoService
     {
+        void Apagar(int id);
+        void Cadastrar(TipoProduto tipoProduto);
+        void Editar(TipoProduto tipoProduto);
+        List<TipoProduto> ObterTodos();
+        TipoProduto ObterPorId(int id);
     }
 }
